358f1ce490fa6e2dbc7e1b3058d92149c60ee32f
mricarte <mricarte@itba.edu.ar>
2022-03-27 23:48:07 -0300

    add runEvolution template, and private methods

--- TP2/src/main/java/sia/grupo19/Genetic.java
+++ TP2/src/main/java/sia/grupo19/Genetic.java
@@ -1,20 +1,30 @@
 package sia.grupo19;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Genetic {
 
 	private static final int X_LENGTH = 11;
 
+	// parameters
 	private int generations;
 	private String crossingMethod;
 	private double mutationProb;
 	private double deviation;
 	private String selectionMethod;
+	private int P;
+
+	private int generationCount = 0;
 	private List<Individuo> population;
+	private List<Individuo> lastPopulation = null;
 
 	public Genetic(int P, int generations, String cross, double prob, double sigma, String selection) {
+		this.P = P;
 		this.generations = generations;
 		this.crossingMethod = cross;
 		this.mutationProb = prob;
@@ -26,9 +36,11 @@ public class Genetic {
 	}
 
 	private void calculateFitness() {
-		for (Individuo ind : population) {
-			ind.getFitness();
-		}
+		/*
+		 * for (Individuo ind : population) {
+		 * ind.getFitness();
+		 * }
+		 */
 		// ordenar la poblacion por fitness
 	}
 
@@ -42,6 +54,48 @@ public class Genetic {
 			}
 			population.add(new Individuo(possibleX));
 		}
+	}
+
+	public Individuo runEvolution() {
+		Individuo best = null;
+		CutOff cutOff = new CutOff(500, 0.01, 10, 8);
+		while (!cutOff.isDone(generationCount, getBestFitness(population),
+				getSharedCount(population, lastPopulation))) {
+			List<Individuo> next = new ArrayList<>();
+
+			lastPopulation = new ArrayList<>(population); // COPY THEM BEFORE MODIFYING THE POOL
+
+			// LOG LAST POPULATION
+
+			while (population.size() < 2 * P) {
+				// PICK 2 from population
+				// get 2 new individuos using Cruza(i1, i2)
+				// MUTATE the 2 new ones
+				// add them to the population? to next?
+
+				// can parents be repeated? can their children be picked? PREGUNTAR
+			}
+
+			// SELECT P individuos from population to pass on to next
+
+			population = next; // pisar population con next
+		}
+
+		// LOG FINAL POPULATION
+
+		// cut off said we are done, return best one
+		return Utils.getBestIndividuo(population);
+	}
+
+	private int getSharedCount(List<Individuo> currPop, List<Individuo> lastPop) {
+		if (lastPop != null) {
+			return Utils.getIntersection(currPop, lastPop).size();
+		}
+		return 0;
+	}
+
+	private double getBestFitness(List<Individuo> pop) {
+		return Utils.getBestIndividuo(pop).getFitness();
 	}
 
 }
