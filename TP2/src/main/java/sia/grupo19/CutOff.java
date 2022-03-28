50ce600c9f268098eedf70a31fdaa6cad065de7b
mricarte <mricarte@itba.edu.ar>
2022-03-27 23:46:54 -0300

    cutoff definition

---
+++ TP2/src/main/java/sia/grupo19/CutOff.java
@@ -0,0 +1,59 @@
+package sia.grupo19;
+
+public class CutOff {
+
+    private int maxGenCount;
+    private double minAcceptable;
+
+    private int SHARED_EPSILON = 50; // make this number into a parameter?
+    private int lastSharedGenStructure = 0; // initially no shared individuals
+    private int repeatedGenStructureCount = 0;
+    private int maxRepeatedGenStructure;
+
+    private double EPSILON = 0.0001; // make this number into a parameter?
+    private double lastBestFitness = -1000; // initially, any best fitness should be good
+    private int repeatedGenContentCount = 0;
+    private int maxRepeatedGenContent;
+
+    public CutOff(int maxGenCount, double minAcceptable, int maxRepeatedGenStructure, int maxRepeatedGenContent) {
+        this.maxGenCount = maxGenCount;
+        this.minAcceptable = minAcceptable;
+        this.maxRepeatedGenStructure = maxRepeatedGenStructure;
+        this.maxRepeatedGenContent = maxRepeatedGenContent;
+    }
+
+    public boolean isDone(int currGen, double currBestFitness, int sharedGenStructure) {
+        return isMaxGen(currGen) || isAcceptable(currBestFitness) || isMaxRepeatedContent(currBestFitness)
+                || isMaxRepeatedStructure(sharedGenStructure);
+    }
+
+    private boolean isMaxRepeatedStructure(int sharedGenStructure) {
+        int diff = Math.abs(sharedGenStructure - lastSharedGenStructure);
+        if (diff >= SHARED_EPSILON) {
+            repeatedGenStructureCount++;
+        } else {
+            repeatedGenStructureCount = 0; // reset
+        }
+
+        return repeatedGenStructureCount >= maxRepeatedGenStructure;
+    }
+
+    private boolean isMaxRepeatedContent(double currBestFitness) {
+        double diff = Math.abs(currBestFitness - lastBestFitness);
+        if (diff < EPSILON) {
+            repeatedGenContentCount++;
+        } else {
+            repeatedGenContentCount = 0; // reset
+        }
+        return repeatedGenContentCount >= maxRepeatedGenContent;
+    }
+
+    private boolean isAcceptable(double currBestFitness) {
+        return currBestFitness >= minAcceptable;
+    }
+
+    private boolean isMaxGen(int currGen) {
+        return currGen >= maxGenCount;
+    }
+
+}
