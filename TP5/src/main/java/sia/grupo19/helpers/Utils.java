bada730c9c771a38605d1743cd97c6767efec51c
mricarte <mricarte@itba.edu.ar>
2022-06-20 23:55:07 -0300

    1a  done

--- TP5/src/main/java/sia/grupo19/helpers/Utils.java
+++ TP5/src/main/java/sia/grupo19/helpers/Utils.java
@@ -1,39 +1,41 @@
 package sia.grupo19.helpers;
 
 public class Utils {
-	
-	private final static int LATENT_SIZE= 20;
-	private final static int DOWN_LIMIT= -1;
-	private final static int UP_LIMIT= 1;
+
+	private final static int LATENT_SIZE = 20;
+	private final static int DOWN_LIMIT = -1;
+	private final static int UP_LIMIT = 1;
 
 	public static double[][] getTestingLatent() {
-		int latentLimit= LATENT_SIZE * LATENT_SIZE;
-		double[][] toRet= new double[latentLimit + 2 * LATENT_SIZE][2];
+		int latentLimit = LATENT_SIZE * LATENT_SIZE;
+		double[][] toRet = new double[latentLimit + 2 * LATENT_SIZE + 1][2];
 
-		double step= (double) (UP_LIMIT - DOWN_LIMIT)/LATENT_SIZE;
-		double x= DOWN_LIMIT;
-		double y= DOWN_LIMIT;
-		int pos= 0;
+		double step = (double) (UP_LIMIT - DOWN_LIMIT) / LATENT_SIZE;
+		double x = DOWN_LIMIT;
+		double y = DOWN_LIMIT;
+		int pos = 0;
 
 		while (pos <= latentLimit) {
-			x= DOWN_LIMIT;
+			x = DOWN_LIMIT;
 			for (int j = 0; j <= LATENT_SIZE; j++) {
-				toRet[pos][0]= x;
-				toRet[pos][1]= y;
-				x+= step;
+				toRet[pos][0] = x;
+				toRet[pos][1] = y;
+				x += step;
 				pos++;
 			}
-			y+= step;
+			y += step;
 		}
-		
-		x= DOWN_LIMIT;
+
+		x = DOWN_LIMIT;
 		for (int j = 0; j < LATENT_SIZE; j++) {
-			toRet[pos][0]= x;
-			toRet[pos][1]= y;
-			x+= step;
+			toRet[pos][0] = x;
+			toRet[pos][1] = y;
+			x += step;
 			pos++;
 		}
-		
+		toRet[pos][0] = 1;
+		toRet[pos][1] = 1;
+
 		return toRet;
 	}
 
--- TP5/src/test/java/sia/grupo19/LatentesTest.java
+++ TP5/src/test/java/sia/grupo19/LatentesTest.java
@@ -62,4 +62,42 @@ public class LatentesTest {
          */
     }
 
+    @Test
+    public void autoEncoderNuevasMuestras() throws Exception {
+        ParamsParser parser = new ParamsParser("./resources/1aconfs/conf9.json", true);
+        MultiLayerPerceptron mlp = new MultiLayerPerceptron(parser.getParams());
+        mlp.run3_3();
+
+        double[][] trainingData = parser.getParams().getTrainingDataInputs();
+
+        double[][] expectedOutputs = mlp.runInputs(trainingData);
+
+        MultiLayerPerceptron encoder = mlp.getEncoder();
+        double[][] encoderOutputs = encoder.runInputs(trainingData);
+
+        MultiLayerPerceptron decoder = mlp.getDecoder();
+        double[][] decoderOutputs = decoder.runInputs(encoderOutputs);
+
+        double[][] testLatent = Utils.getTestingLatent();
+        // double[][] testtt = { { 0.8999999999999998, 0.9999999999999998 } };
+        double[][] latenteOutput = decoder.runInputs(testLatent);
+
+        /*
+         *
+         * System.out.println("Expected:");
+         * System.out.println(new Gson().toJson(expectedOutputs));
+         * System.out.println("Result:");
+         * System.out.println(new Gson().toJson(decoderOutputs));
+         * System.out.println("Inputs:");
+         * System.out.println(new Gson().toJson(trainingData));
+         */
+        System.out.println("Latente:");
+        System.out.println(new Gson().toJson(encoderOutputs));
+
+        System.out.println("TestLatent:");
+        System.out.println(new Gson().toJson(testLatent));
+        System.out.println("New char:");
+        System.out.println(new Gson().toJson(latenteOutput));
+    }
+
 }
